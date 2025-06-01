a0009b7f839ad81411f449d8e4097e3b9b4116af
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-06-01 10:10:12 +0100

    feat: image nd category controller

--- src/main/java/com/danztee/quickbuy/controller/CategoryController.java
+++ src/main/java/com/danztee/quickbuy/controller/CategoryController.java
@@ -1,4 +1,54 @@
 package com.danztee.quickbuy.controller;
 
+import com.danztee.quickbuy.model.Category;
+import com.danztee.quickbuy.response.ApiResponse;
+import com.danztee.quickbuy.service.category.CategoryService;
+import com.danztee.quickbuy.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
+    private final ICategoryService categoryService;
+
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Success", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+
+        try {
+            Category category = categoryService.AddCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Success", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+
+    @GetMapping("/category/${id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Success", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
